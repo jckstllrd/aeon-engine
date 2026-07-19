037540d59c9b27a8638539fc32a70c08a01b4313
jckstllrd <jackstallard17@gmail.com>
2026-07-19 11:04:51 +0100

    fix: set opengl forward compatibility to true to ensure no deprecated functions available

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "aeon-engine", NULL, NULL);
     if (window == NULL) {
